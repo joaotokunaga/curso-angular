5e24474d9e61fd54191164f95e99c5d94463661d
João Tokunaga <joaotokunaga@gmail.com>
2022-04-26 11:37:05 -0300

    Renderização de listas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
 @NgModule({
 	declarations: [
@@ -21,6 +22,7 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
   EventosComponent,
   EmitterComponent,
   ChangeNumberComponent,
+  ListRenderComponent,
 	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
