9af557b5b44e3a28cde2f3e07c1630d3e9f9e0af
João Tokunaga <joaotokunaga@gmail.com>
2022-04-26 13:52:02 -0300

    Utilizando parâmetros da URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 	{ path: '', component: FirstComponentComponent },
 	{ path: 'list', component: ListRenderComponent },
+	{ path: 'list/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
---
+++ src/app/components/item-detail/item-detail.component.ts
@@ -0,0 +1,28 @@
+import { ListService } from 'src/app/services/list.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+import { Animal } from 'src/app/interfaces/Animal';
+
+@Component({
+	selector: 'app-item-detail',
+	templateUrl: './item-detail.component.html',
+	styleUrls: ['./item-detail.component.css']
+})
+export class ItemDetailComponent implements OnInit {
+
+	// animal é opcional
+	animal?: Animal
+
+	constructor(private listService: ListService, private route: ActivatedRoute) {
+		this.getAnimal()
+	}
+
+	ngOnInit(): void {
+	}
+
+	getAnimal() {
+		const id = Number(this.route.snapshot.paramMap.get("id"))
+		this.listService.getItem(id).subscribe((animal) => this.animal = animal)
+	}
+}
